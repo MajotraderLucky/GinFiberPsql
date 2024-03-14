dc99c6a3b98d656921f967433d9d02361a8461e9
MajotraderLucky <majohead@gmail.com>
2024-03-14 19:25:43 +0300

    feat(main): Create a function that makes a query using a composite index to a postgresql database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"ginfiberpsql/checkdb"
 	"log"
 	"time"
@@ -8,6 +9,33 @@ import (
 	"github.com/MajotraderLucky/Utils/logger"
 	_ "github.com/lib/pq"
 )
+
+// Function to execute a query using a composite index
+func queryPersonByNameAndSurname(db *sql.DB, name, surname string) (int, string, string, string, int, string, string, error) {
+	// The query uses the composite index idx_fio_data_name_surname
+	query := `SELECT id, name, surname, patronymic, age, gender, nationality FROM fio_data WHERE name = $1 AND surname = $2;`
+
+	// Executing the query
+	row := db.QueryRow(query, name, surname)
+
+	// Reading the results
+	var (
+		id            int
+		resultName    string
+		resultSurname string
+		patronymic    string
+		age           int
+		gender        string
+		nationality   string
+	)
+
+	err := row.Scan(&id, &resultName, &resultSurname, &patronymic, &age, &gender, &nationality)
+	if err != nil {
+		return 0, "", "", "", 0, "", "", err
+	}
+
+	return id, resultName, resultSurname, patronymic, age, gender, nationality, nil
+}
 
 func main() {
 	// Wait for other services to be ready
@@ -34,8 +62,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Close the database connection
+	// Close the database connection at the end
 	defer db.Close()
+
+	// Filter values
+	name := "Иван"
+	surname := "Иванов"
+
+	// Using the function to execute the query
+	id, resultName, resultSurname, patronymic, age, gender, nationality, err := queryPersonByNameAndSurname(db, name, surname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("ID: %d, Name: %s, Surname: %s, Patronymic: %s, Age: %d, Gender: %s, Nationality: %s\n",
+		id, resultName, resultSurname, patronymic, age, gender, nationality)
 
 	logger.CleanLogCountLines(50)
 
--- postgrdb/init.sql
+++ postgrdb/init.sql
@@ -1,3 +1,4 @@
+-- Creating the fio_data table
 CREATE TABLE fio_data (
     id SERIAL PRIMARY KEY,
     name TEXT,
@@ -11,4 +12,8 @@ CREATE TABLE fio_data (
     updated_at TIMESTAMP DEFAULT current_timestamp
 );
 
+-- Creating an index on the name field
 CREATE INDEX idx_fio_data_name ON fio_data (name);
+
+-- Creating a composite index on the name and surname fields
+CREATE INDEX idx_fio_data_name_surname ON fio_data (name, surname);
