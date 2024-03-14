7a604be9b6cd0d2870f3e66e9f36b0671d43f36c
MajotraderLucky <majohead@gmail.com>
2024-03-14 18:06:42 +0300

    feat(main): Add logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/MajotraderLucky/Utils/logger"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	logger := logger.Logger{}
+	err := logger.CreateLogsDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = logger.OpenLogFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.SetLogger()
+	logger.LogLine()
+
+	log.Println("GinFiberPsql started...")
+	logger.LogLine()
 }
