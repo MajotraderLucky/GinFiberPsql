465ad6aa536ef86f4125b22b1f75c4c113ff9ca3
MajotraderLucky <majohead@gmail.com>
2024-03-15 12:19:55 +0300

    feat(apigin): Add logger for the apigin package

--- apigin/apigin.go
+++ apigin/apigin.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/MajotraderLucky/Utils/logger"
+)
 
 func main() {
-	fmt.Println("Gin started...")
+	logger := logger.Logger{}
+	err := logger.CreateLogsDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = logger.OpenLogFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger.SetLogger()
+	logger.LogLine()
+	log.Println("Gin started...")
+	logger.LogLine()
 }
